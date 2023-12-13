71988a0b601e01687fda58ddbcce74d13c9c38e1
Mukesh Bhairewar <mukeshbhairewar96@gmail.com>
2023-12-14 00:29:46 +0530

    Add project files.

---
+++ Xml To Json Converter/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Xml_To_Json_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            textBox1 = new TextBox();
+            browsebutton_Click = new Button();
+            convertbutton_click = new Button();
+            openfilebutton_click = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(194, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(288, 31);
+            label1.TabIndex = 0;
+            label1.Text = "XML To JSON Converter";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(172, 122);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(250, 27);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // browsebutton_Click
+            // 
+            browsebutton_Click.FlatAppearance.BorderColor = Color.Black;
+            browsebutton_Click.FlatAppearance.BorderSize = 2;
+            browsebutton_Click.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            browsebutton_Click.Location = new Point(447, 120);
+            browsebutton_Click.Name = "browsebutton_Click";
+            browsebutton_Click.Size = new Size(94, 29);
+            browsebutton_Click.TabIndex = 2;
+            browsebutton_Click.Text = "Browse";
+            browsebutton_Click.UseVisualStyleBackColor = true;
+            browsebutton_Click.Click += button1_Click;
+            // 
+            // convertbutton_click
+            // 
+            convertbutton_click.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            convertbutton_click.Location = new Point(172, 210);
+            convertbutton_click.Name = "convertbutton_click";
+            convertbutton_click.Size = new Size(94, 29);
+            convertbutton_click.TabIndex = 3;
+            convertbutton_click.Text = "Convert";
+            convertbutton_click.UseVisualStyleBackColor = true;
+            convertbutton_click.Click += button2_Click;
+            // 
+            // openfilebutton_click
+            // 
+            openfilebutton_click.FlatAppearance.BorderColor = Color.Maroon;
+            openfilebutton_click.FlatAppearance.BorderSize = 4;
+            openfilebutton_click.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            openfilebutton_click.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            openfilebutton_click.Location = new Point(339, 210);
+            openfilebutton_click.Name = "openfilebutton_click";
+            openfilebutton_click.Size = new Size(143, 29);
+            openfilebutton_click.TabIndex = 4;
+            openfilebutton_click.Text = "OpenJsonFile";
+            openfilebutton_click.UseVisualStyleBackColor = true;
+            openfilebutton_click.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(800, 450);
+            Controls.Add(openfilebutton_click);
+            Controls.Add(convertbutton_click);
+            Controls.Add(browsebutton_Click);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox1;
+        private Button browsebutton_Click;
+        private Button convertbutton_click;
+        private Button openfilebutton_click;
+    }
+}
---
+++ Xml To Json Converter/Form1.cs
@@ -0,0 +1,199 @@
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Xml_To_Json_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the OpenFileDialog class
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set the title and filter for the dialog
+            openFileDialog1.Title = "Select XML File";
+            openFileDialog1.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+
+            // Show the dialog and get the result
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            // If the user clicks OK, update the TextBox with the selected file name
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Convert XML to JSON with indentation
+            string xmlFilePath = textBox1.Text;
+
+            if (File.Exists(xmlFilePath))
+            {
+                try
+                {
+                    XmlReaderSettings settings = new XmlReaderSettings
+                    {
+                        IgnoreWhitespace = true
+                    };
+
+                    using (XmlReader reader = XmlReader.Create(xmlFilePath, settings))
+                    {
+                        XDocument xmlDocument = XDocument.Load(reader);
+
+                        // Trim whitespace from text content of XML elements
+                        TrimWhitespace(xmlDocument.Root);
+
+                        string json = JsonConvert.SerializeObject(xmlDocument, Newtonsoft.Json.Formatting.Indented);
+
+                        // Specify the path to save the formatted JSON file
+                        string jsonFilePath = Path.ChangeExtension(xmlFilePath, ".json");
+
+                        // Save the formatted JSON content to a file
+                        File.WriteAllText(jsonFilePath, json);
+
+                        MessageBox.Show("Conversion successful. JSON file saved at: " + jsonFilePath, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error converting XML to JSON: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid XML file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TrimWhitespace(XElement element)
+        {
+            foreach (XNode node in element.Nodes().ToList())
+            {
+                if (node is XText text)
+                {
+                    text.Value = text.Value.Trim();
+                    if (string.IsNullOrEmpty(text.Value))
+                    {
+                        node.Remove();
+                    }
+                }
+                else if (node is XElement childElement)
+                {
+                    TrimWhitespace(childElement);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Convert XML to JSON with indentation
+            string xmlFilePath = textBox1.Text;
+
+            if (File.Exists(xmlFilePath))
+            {
+                try
+                {
+                    // Specify the path to the corresponding JSON file
+                    string jsonFilePath = Path.ChangeExtension(xmlFilePath, ".json");
+
+                    if (File.Exists(jsonFilePath))
+                    {
+                        // Read the contents of the JSON file
+                        using (StreamReader sr = new StreamReader(jsonFilePath))
+                        {
+                            string jsonContent = sr.ReadToEnd();
+
+                            // Create a new form to display the JSON content with syntax highlighting
+                            using (Form jsonForm = new Form())
+                            {
+                                RichTextBox richTextBox = new RichTextBox
+                                {
+                                    Multiline = true,
+                                    ReadOnly = true,
+                                    Dock = DockStyle.Fill,
+                                    Text = jsonContent
+                                };
+
+                                // Colorize JSON syntax
+                                ColorizeJsonSyntax(richTextBox);
+
+                                jsonForm.Controls.Add(richTextBox);
+
+                                jsonForm.ShowDialog();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("The corresponding JSON file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error reading JSON file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid XML file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ColorizeJsonSyntax(RichTextBox richTextBox)
+        {
+            // Define regular expressions for JSON syntax highlighting
+            var jsonKeywords = new Regex(@"\b(?:true|false|null)\b", RegexOptions.Compiled);
+            var jsonString = new Regex("\"(?:[^\"\\\\]|\\\\.)*\"", RegexOptions.Compiled);
+            var jsonNumbers = new Regex(@"\b(?:\d+\.\d*|\.\d+|\d+)\b", RegexOptions.Compiled);
+            var jsonOperators = new Regex(@"[\{\}\[\]:,]", RegexOptions.Compiled);
+
+            // Set default color for the text
+            richTextBox.SelectionColor = richTextBox.ForeColor;
+
+            // Apply syntax highlighting based on regular expressions
+            foreach (Match match in jsonKeywords.Matches(richTextBox.Text))
+            {
+                richTextBox.Select(match.Index, match.Length);
+                richTextBox.SelectionColor = Color.Blue; // Change color for keywords
+            }
+
+            foreach (Match match in jsonString.Matches(richTextBox.Text))
+            {
+                richTextBox.Select(match.Index, match.Length);
+                richTextBox.SelectionColor = Color.DarkRed; // Change color for strings
+            }
+
+            foreach (Match match in jsonNumbers.Matches(richTextBox.Text))
+            {
+                richTextBox.Select(match.Index, match.Length);
+                richTextBox.SelectionColor = Color.Green; // Change color for numbers
+            }
+
+            foreach (Match match in jsonOperators.Matches(richTextBox.Text))
+            {
+                richTextBox.Select(match.Index, match.Length);
+                richTextBox.SelectionColor = Color.Black; // Change color for operators
+            }
+
+            // Reset selection to prevent the last character from being colored
+            richTextBox.Select(richTextBox.TextLength, 0);
+            richTextBox.SelectionColor = richTextBox.ForeColor;
+        }
+    }
+
+}
